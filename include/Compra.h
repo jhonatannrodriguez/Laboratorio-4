238e939abab1bc0dfa69781121cd13b57b76f4e9
AndresFeijo <163592544+AndresFeijo@users.noreply.github.com>
2024-06-07 15:45:06 -0300

    Compra primer intento

---
+++ include/Compra.h
@@ -0,0 +1,23 @@
+#ifndef _COMPRA_H
+#define _COMPRA_H
+
+using namespace std;
+#include "DTFecha.h"
+#include <string>
+class Compra
+{
+private:
+    DTFecha fecha;
+    float monto; 
+public:
+    Compra();
+    Compra(DTFecha fecha,float monto);
+    void setMonto(float);
+    void setFecha(DTFecha);
+    float getMonto();
+    DTFecha getFecha();
+    
+  
+};
+
+#endif
