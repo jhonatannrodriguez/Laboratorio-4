e6c8e558293c542db5f7d6315145063ff9a149dc
jhonatannrodriguez <elezerodriguezalvez@hotmail.com>
2024-06-08 01:44:11 -0300

    DTPromocion

---
+++ include/DTPromocion.h
@@ -0,0 +1,27 @@
+#ifndef DTPROMOCION_H
+#define DTPROMOCION_H
+
+#include "DTFecha.h"
+#include <string>
+
+using namespace std;
+
+class DTPromocion {
+    private:
+        DTFecha vencimiento;
+        string descripcion;
+        string nombre;
+        float  descuento;
+    public:
+        DTPromocion();
+        DTFecha(DTFecha, string, string, float);
+        DTFecha getVencimiento();
+        string getDescripcion();
+        string getNombre();
+        float getDescuento();
+
+        ~DTPromocion();
+
+} ;
+
+#endif
