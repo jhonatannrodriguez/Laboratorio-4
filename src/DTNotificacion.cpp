0412febf964896e67629c87ce1e89003cbceef60
Daniel-Feijo <andresfeijosilva@gmail.com>
2024-06-11 10:56:27 -0300

    Arreglos en DTNotificacion
    Son dos :: y habia solo un :

--- src/DTNotificacion.cpp
+++ src/DTNotificacion.cpp
@@ -2,30 +2,32 @@
 
 DTNotificacion::DTNotificacion() {
     this->nombrePromo = "";
-    this->productos;
+    this->productos; // Como inicializo un set
     this->descuento = 0;
     this->vencimiento = DTFecha(0,0,0);
 }
 
-DTNotificacion::DTNotificacion(string nombrePromo, set<string> productos, float descuento,  DTFecha vencimiento) {
-    this->nombrePromo = getNombrePromo;
+DTNotificacion :: DTNotificacion(string nombrePromo, set<string> productos, float descuento,  DTFecha vencimiento) {
+    this->nombrePromo = nombrePromo;
     this->productos = productos;
     this->descuento = descuento;
     this->vencimiento = vencimiento;
 }
 
-string DTNotificacion:getNombrePromo() {
-    return = this->nombrePromo;
+string DTNotificacion :: getNombrePromo() {
+    return this->nombrePromo;
 }
 
-set<string> DTNotificacion:getProductos() {
-    return = this->productos;
+set<string> DTNotificacion :: getProductos() {
+    return this->productos;
 }
 
-float DTNotificacion:getDescuento() {
-    return = this->descuento;
+float DTNotificacion :: getDescuento() {
+    return this->descuento;
 }
 
-DTFecha DTNotificacion:getVencimiento() {
-    return = this->vencimiento;
+DTFecha DTNotificacion :: getVencimiento() {
+    return this->vencimiento;
 }
+
+DTNotificacion :: ~DTNotificacion() {} // Llevan destructor los dt?
