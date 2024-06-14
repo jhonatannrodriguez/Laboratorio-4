0cce88e911122ac6b73d502d0c3cf359d6805083
AndresFeijo <163592544+AndresFeijo@users.noreply.github.com>
2024-06-14 15:14:12 -0300

    IProducto

--- include/IProducto.h
+++ include/IProducto.h
@@ -2,11 +2,17 @@
 #define _IPRODUCTO_H
 
 #include "DTFecha.h"
+#include "DTProductoInfo.h"
+#include "DTProducto.h"
+#include "Producto.h"
 using namespace std;
 #include <string>
+#include <set>
+#include <map>
 
 class IProducto {
 public:
-    
+    virtual set<DTProducto> consultarProducto();
+    virtual DTProductoInfo seleccionarProducto(string codigo);
 };
 #endif
