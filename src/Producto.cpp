78afcfdfd067dc160955f10991324db85a167ee1
IsmaelBorroy <iborroy2003@gmail.com>
2024-06-23 12:20:00 -0300

    cambios

--- src/Producto.cpp
+++ src/Producto.cpp
@@ -149,11 +149,8 @@
 
     void Producto :: eliminarComentario(string id) {
         //Se le pasa una id válida
-        cout << "Encontrar ID" << endl;
         map<string, Comentario*>::iterator c = this->comentarios.find(id);
-        cout << "EntrarAlIf->" << endl;
         if (c != this->comentarios.end()) {
-            cout << "Erase->" << endl;
             this->comentarios.erase(id);
         }
     }
